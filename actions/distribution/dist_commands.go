706e7abe5e2df81142d9e0ee419bf8211acec184
Heath Kuntz <heath@ari-water.com>
2021-04-19 16:57:30 -0600

    Added weather stations function and added coordinates to cells for distance calculations.

--- actions/distribution/dist_commands.go
+++ actions/distribution/dist_commands.go
@@ -3,6 +3,7 @@ package distribution
 import (
 	"clibasic/color"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/lib/pq"
 )
@@ -16,10 +17,21 @@ const (
 )
 
 type actCell struct {
-	Rw        int
-	Clm       int
-	SoilCode  int
-	TfgCellId int
+	Rw       int
+	Clm      int
+	SoilCode int
+	CellId   int
+	cor      coord
+}
+
+type coord struct {
+	T           string    `json:"type"`
+	Coordinates []float64 `json:"coordinates"`
+}
+
+type weatherStation struct {
+	Code string `json:"code"`
+	Cor  coord  `json:"location"`
 }
 
 func Distribution(debug *bool, startYr *int, endYr *int) {
@@ -45,30 +57,46 @@ func Distribution(debug *bool, startYr *int, endYr *int) {
 
 	fmt.Println("Connected!")
 
-	rows, err := db.Query(`SELECT rw, clm, soil_code, tfg_cellid FROM public.act_cells`)
+	rows, err := db.Query(`SELECT tfg_cellid as cellid, rw, clm, soil_code, 
+       st_asgeojson(st_transform(st_centroid(geom), 4326)) as cent FROM public.act_cells;`)
+
 	if err != nil {
 		panic(err)
 	}
 
-	defer rows.Close()
-
 	var Cells []actCell
 	cell := actCell{}
 	for rows.Next() {
-		var rw, clm, soil, cellid int
-		err = rows.Scan(&rw, &clm, &soil, &cellid)
+		var cellid, rw, clm, soil int
+		var c []byte
+		var cor coord
+
+		err = rows.Scan(&cellid, &rw, &clm, &soil, &c)
 		if err != nil {
 			panic(err.Error())
 		}
+
+		err := json.Unmarshal(c, &cor)
+		if err != nil {
+			fmt.Println("error", err)
+		}
+
 		cell.Rw = rw
 		cell.Clm = clm
 		cell.SoilCode = soil
-		cell.TfgCellId = cellid
+		cell.CellId = cellid
+		cell.cor = cor
 		Cells = append(Cells, cell)
 	}
 
+	wStations := getWeatherStations(db)
+
 	for _, c := range Cells[:5] {
 		fmt.Println(c)
+	}
+
+	for _, w := range wStations {
+		fmt.Println(w)
 	}
 }
 
@@ -76,4 +104,35 @@ func CheckError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func getWeatherStations(db *sql.DB) []weatherStation {
+	rows, err := db.Query(`SELECT code, st_asgeojson(st_transform(geom, 4326)) as location FROM public.weather_stations;`)
+	if err != nil {
+		panic(err)
+	}
+
+	var wStations []weatherStation
+	for rows.Next() {
+		station := weatherStation{}
+		var code string
+		var c []byte
+		var cor coord
+
+		err = rows.Scan(&code, &c)
+		if err != nil {
+			fmt.Println("error", err)
+		}
+
+		err := json.Unmarshal(c, &cor)
+		if err != nil {
+			fmt.Println("error", err)
+		}
+
+		station.Code = code
+		station.Cor = cor
+		wStations = append(wStations, station)
+	}
+
+	return wStations
 }
