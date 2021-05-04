406bde1f6af62e99369f00a3477194a5daffb096
Heath Kuntz <heath@ari-water.com>
2021-05-04 15:36:30 -0600

    Added results filters and loop for parcels. Also added progress bar for the cells loop.

--- actions/rchCommands.go
+++ actions/rchCommands.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"fmt"
+	"github.com/schollz/progressbar"
 	"wwum2020/database"
 	"wwum2020/rchFiles"
 	//"wwum2020/rchFiles"
@@ -15,11 +16,6 @@ func RechargeFiles(debug *bool, startYr *int, endYr *int, CSDir *string) {
 	// load up data with cell acres
 	cells := database.GetCells(pgDb)
 
-	// loop through the cells
-	for _, cell := range cells[:5] {
-		fmt.Println(cell.CellId)
-	}
-
 	// will also need parcel sw delivery, gw pumping (if available), distributed nir, rf, eff precip for the required crops
 
 	// Natural Veg 102
@@ -27,12 +23,44 @@ func RechargeFiles(debug *bool, startYr *int, endYr *int, CSDir *string) {
 
 	// Irr Cells
 	irrCells := rchFiles.GetCellsIrr(pgDb, 2014)
-	fmt.Println("First Irrigated Cell:")
-	fmt.Println(irrCells[0])
+	//fmt.Println("First Irrigated Cell:")
+	//fmt.Println(irrCells[0])
 
 	// Dry Cells
 	dryCells := rchFiles.GetDryCells(pgDb, 2014)
-	fmt.Println("First Dry Cell:")
-	fmt.Println(dryCells[0])
+	//fmt.Println("First Dry Cell:")
+	//fmt.Println(dryCells[0])
 
+	// progress bar
+	bar := progressbar.Default(int64(len(cells)))
+
+	// loop through the cells
+	for _, cell := range cells {
+		//fmt.Println(cell.CellId)
+
+		var irrCellsResult []rchFiles.IrrCell
+		var dryCellResult []rchFiles.DryCell
+		totalParcelAcres := 0.0
+
+		// filter irrCells for this cell also get acres and add to total
+		for _, ic := range irrCells {
+			if ic.CellId == cell.CellId {
+				irrCellsResult = append(irrCellsResult, ic)
+				totalParcelAcres += ic.IrrArea
+			}
+		}
+
+		// filter dryCells for this cell also get acres and add to total
+		for _, dc := range dryCells {
+			if dc.CellId == cell.CellId {
+				dryCellResult = append(dryCellResult, dc)
+				totalParcelAcres += dc.DryArea
+			}
+		}
+
+		if cell.CellId == 78585 {
+			fmt.Printf("CellId: %d, Total Parcel Acres: %g\n", cell.CellId, totalParcelAcres)
+		}
+		bar.Add(1)
+	}
 }
