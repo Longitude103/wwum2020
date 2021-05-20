3344ccd4b5a2c7126c6b2eb3192d4156c67b556e
heath@ari-water.com <heath@ari-water.com>
2021-05-20 09:04:20 -0600

    Added documentation to pumping.go file and changed calculation so the nir was multiplied by acres to estimate pumping.

--- parcelpump/pumping.go
+++ parcelpump/pumping.go
@@ -4,7 +4,7 @@ import "github.com/heath140/wwum2020/database"
 
 // estimatePumping is a method that is called on parcels that have metered == false and gw == true so that we can estimate
 // the amount of pumping that was done at the parcel since a well is present, but not metered. Usually FA area before
-// 2017 and other parcels that are not metered. It fills the Usage field of the Parcel struct
+// 2017 and other parcels that are not metered. It fills the Pump field of the Parcel struct
 func (p *Parcel) estimatePumping(cCrops []database.CoeffCrop) {
 	nirAdj := adjustmentFactor(p, cCrops)
 
@@ -18,13 +18,15 @@ func (p *Parcel) estimatePumping(cCrops []database.CoeffCrop) {
 
 	// set nirRemaining to nir - swAvailableCU if positive, then divide by AppEff to arrive at pumping
 	for m := 0; m < 12; m++ {
-		nirRemaining[m] = p.Nir[m] - swAvailableCU[m]
+		nirRemaining[m] = (p.Nir[m] * p.Area) - swAvailableCU[m]
 		if nirRemaining[m] > 0 {
 			p.Pump[m] = nirRemaining[m] / p.AppEff
 		}
 	}
 }
 
+// adjustmentFactor function calculates the Parcel adjustment factor by weighting the crops and distribution of the
+// crops in a Parcel by calling the nirFactor and then weighting it based on crop distribution
 func adjustmentFactor(p *Parcel, cCrops []database.CoeffCrop) float64 {
 	var c1, c2, c3, c4 float64
 	c1 = nirFactor(cCrops, p.CoeffZone, int(p.Crop1.Int64)) * p.Crop1Cov.Float64
@@ -43,6 +45,8 @@ func adjustmentFactor(p *Parcel, cCrops []database.CoeffCrop) float64 {
 	return c1 + c2 + c3 + c4
 }
 
+// nirFactor is a filter function that returns the NirAdjFactor from the CoeffCrop slice and limits it to the zone of the
+// Parcel and the crop type.
 func nirFactor(cCrops []database.CoeffCrop, zone int, crop int) (nf float64) {
 	for _, v := range cCrops {
 		if v.Zone == zone && v.Crop == crop {
