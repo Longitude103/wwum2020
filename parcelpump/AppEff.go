047ef19f930cb7674b0f728367d7241d9950d53c
heath@ari-water.com <heath@ari-water.com>
2021-05-20 08:54:25 -0600

    Added documentation to AppEff.go file.

--- parcelpump/AppEff.go
+++ parcelpump/AppEff.go
@@ -2,6 +2,8 @@ package parcelpump
 
 import "github.com/heath140/wwum2020/database"
 
+// setAppEfficiency is a Parcel method that uses the parcel IrrType and year to determine the application efficiency of the
+// parcel and sets that in the Parcel struct. It takes a slice of Efficiency and year as they very through the study period.
 func (p *Parcel) setAppEfficiency(efficiencies []database.Efficiency, year int) {
 	f, s := filterEff(efficiencies, year)
 
@@ -12,6 +14,7 @@ func (p *Parcel) setAppEfficiency(efficiencies []database.Efficiency, year int) 
 	}
 }
 
+// filterEff returns the efficiency of flood and sprinkler for a given year.
 func filterEff(efficiencies []database.Efficiency, year int) (f float64, s float64) {
 	for _, v := range efficiencies {
 		if v.Yr == year {
