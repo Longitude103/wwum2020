5952ef0d9d0beecadda1a158fd2dd43c9da04431
heath140 <heath@ari-water.com>
2021-09-03 09:55:41 -0600

    Bug fix, moved close DB to under SSWells.

--- actions/rModelCommands.go
+++ actions/rModelCommands.go
@@ -95,13 +95,12 @@ func RunModel(debug bool, CSDir *string, sY int, eY int, eF bool, myEnv map[stri
 		return err
 	}
 
-	_ = v.SlDb.Close() // close the db before ending the program
-
 	// run steady State Wells
 	if err := wells.SteadyStateWells(v); err != nil {
 		return err
 	}
 
+	_ = v.SlDb.Close() // close the db before ending the program
 	v.Logger.Infof("Model Run took: %s", time.Now().Sub(timeStart))
 	v.Logger.Info("Model Completed without Error")
 	fmt.Println("Model Completed without Error.")
