1cbd6a2f98fb3256c04cdf255e0ae7d7e86a08d5
heath140 <heath@ari-water.com>
2021-10-15 17:19:30 -0600

    Updated Go Documentation

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
+// Copyright 2020 Longitude103. All rights reserved.
+// Use of this source code is governed by GPLv3
+// license that can be found in the LICENSE file.
+
+// Package wwum2020 implements the Western Water Use Management model, 2020 update.
+//
+// The model calculates the crop water use and subsequent
+// soil water balance that is needed to operate the MODFLOW
+// modeling used in administration decisions by the NRDs of
+// Nebraska. The model is a CLI based model that uses two basic
+// commands to operate it. The first is runModel that
+// tells the model to operate and calculate all the required
+// information and saves it in an output database locally.
+// Once the output database and log files are created, the
+// other command is run mfFiles that produces the MODFLOW
+// WEL and RCH files needed to be imported into that model
+// for operations. There are a series of questions that
+// are asked as to the model options for the output files
+// that allow some flexibility for the creation of the
+// files.
+//
 package main
 
 import (
