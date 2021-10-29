49e910d8c5f3524f3b1632b261c8444e5ca79e36
heath140 <heath@ari-water.com>
2021-10-29 16:50:07 -0600

    File to compile the app in various os platforms

---
+++ compileApp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+GOOS=windows GOARCH=amd64 go build -o bin/wwum2020-amd64.exe
+GOOS=darwin GOARCH=amd64 go build -o bin/wwum2020-amd64-darwin
+GOOS=linux GOARCH=amd64 go build -o bin/wwum2020-amd64-linux
+
+echo "Done Compiling wwum2020, look in bin/"
