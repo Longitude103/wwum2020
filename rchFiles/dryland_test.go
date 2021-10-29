52c94d3365b582040530f7c741276579bbff1802
heath140 <heath@ari-water.com>
2021-10-29 16:50:55 -0600

    Clean up dryland.go and dryland_test.go and delete code no longer used.

--- rchFiles/dryland_test.go
+++ rchFiles/dryland_test.go
@@ -30,50 +30,6 @@ var (
 	sliceDryParcels = []parcels.Parcel{dp1, dp2, dp3, dp4}
 )
 
-func Test_findResult(t *testing.T) {
-	found, location := findResult(sliceR, 234567, time.Date(2021, 7, 1, 0, 0, 0, 0, time.UTC))
-	if found != true || location != 1 {
-		t.Error("should have found record but it didn't")
-	}
-
-	found, location = findResult(sliceR, 1, time.Date(2021, 7, 1, 0, 0, 0, 0, time.UTC))
-	if found == true || location != 0 {
-		t.Error("found a result that there was none to find")
-	}
-
-	found, location = findResult(sliceR, 234567, time.Date(2021, 6, 1, 0, 0, 0, 0, time.UTC))
-	if found == true || location != 0 {
-		t.Error("found a result that there was none to find")
-	}
-}
-
-func Test_inGrouped(t *testing.T) {
-	result := inGrouped(sliceR, 234567)
-	if result != true {
-		t.Error("Should be false, but got true")
-	}
-
-	result = inGrouped(sliceR, 1)
-	if result == true {
-		t.Error("Should be false, but got true")
-	}
-}
-
-func Test_groupResults(t *testing.T) {
-	result := groupResults(sliceR)
-
-	if result[0].Result != 3 {
-		t.Errorf("Grouping not working, expected 3 but got %f", result[0].Result)
-	}
-
-	newSliceR := sliceR[:len(sliceR)-1]
-	result = groupResults(newSliceR)
-
-	if result[0].Result != 1 {
-		t.Errorf("Grouping not working with slice of nothing to group, expected 1 got %f", result[0].Result)
-	}
-}
-
 type AnyTime struct{}
 
 // Match satisfies sqlmock.Argument interface
