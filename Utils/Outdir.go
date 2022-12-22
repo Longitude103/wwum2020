9e47618285f573e05dee60fc4fa55a766b12f9e9
Heath Kuntz <heath@ari-water.com>
2022-12-22 10:46:49 -0700

    removed call to time api and replaced with system time from time.Now()

--- Utils/Outdir.go
+++ Utils/Outdir.go
@@ -1,48 +1,22 @@
 package Utils
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
+	"time"
 )
-
-type tm struct {
-	Dt string `json:"datetime"`
-}
-
-func (t *tm) replaceColon() {
-	t.Dt = strings.Replace(t.Dt, ":", "-", -1)
-}
-
-func (t *tm) trimmed() string {
-	return t.Dt[:len(t.Dt)-13]
-}
 
 // MakeOutputDir makes the output directory, sends the full path and the sub dir value for making files as return values for the function as well as
 // any errors that might have occurred since this hits a world time API.
 func MakeOutputDir(folderTitle string) (string, string, error) {
-	resp, err := http.Get("https://worldtimeapi.org/api/timezone/America/Denver")
-	if err != nil {
-		return "", "", err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", "", err
-	}
-
-	var responseTime tm
-	if err := json.Unmarshal(body, &responseTime); err != nil {
-		fmt.Print("Error getting time for directory")
-	}
-	responseTime.replaceColon()
+	goTime := time.Now()
+	timeString := fmt.Sprint(goTime.Year()) + "-" + fmt.Sprint(goTime.Month()) + "-" + fmt.Sprint(goTime.Day())
+	ts2 := timeString + "T" + fmt.Sprint(goTime.Hour()) + ":" + fmt.Sprint(goTime.Minute()) + ":" + fmt.Sprint(goTime.Second())
+	timeString += "T" + fmt.Sprint(goTime.Hour()) + "-" + fmt.Sprint(goTime.Minute()) + "-" + fmt.Sprint(goTime.Second())
 
 	wd, _ := os.Getwd()
-	rsT := folderTitle + responseTime.trimmed()
+	rsT := folderTitle + timeString
 	subPath := filepath.Join("OutputFiles", rsT)
 	path := filepath.Join(wd, subPath)
 
@@ -50,5 +24,5 @@ func MakeOutputDir(folderTitle string) (string, string, error) {
 		return "", "", err
 	}
 
-	return path, responseTime.trimmed(), nil
+	return path, ts2, nil
 }
