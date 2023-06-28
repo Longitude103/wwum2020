79a8d18985098c105956b68bfc7a8b5d750dd85d
Heath Kuntz <heath@ari-water.com>
2023-06-27 18:01:17 -0600

    Finished Command to Delete a Parcel

--- cmd/parcels.go
+++ cmd/parcels.go
@@ -2,19 +2,75 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/Longitude103/wwum2020/database"
 	"github.com/spf13/cobra"
 )
 
 var parcelDeleteCmd = &cobra.Command{
 	Use:   "parcel-delete",
-	Short: "Delete a parcel",
-	Long:  `Delete a parcel.`,
+	Short: "Delete parcel",
+	Long:  `Delete a parcel along with the junction table values within that parcel.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete parcel called")
-		fmt.Printf("user inside parcel delete: %+v\n", user)
+		yr, _ := cmd.Flags().GetInt("year")
+		parcelId, _ := cmd.Flags().GetInt("id")
+		if parcelId == 0 {
+			fmt.Println("please provide a parcel id")
+			return
+		}
+
+		nrd, _ := cmd.Flags().GetString("nrd")
+		if nrd != "np" && nrd != "sp" {
+			fmt.Println("please provide a valid nrd, must be 'np' or 'sp'")
+			return
+		}
+
+		// get database
+		var opts []database.Option
+		opts = append(opts, database.WithNoSQLite())
+		v, err := database.NewSetup(myEnv, opts...)
+		if err != nil {
+			log.Fatal("Failed to create database: ", err)
+		}
+
+		// look for parcel and tell them if we don't find it
+		query := fmt.Sprintf("select EXISTS(select parcel_id from %s.t%d_irr where parcel_id = %d);", nrd, yr, parcelId)
+
+		var exists bool
+		if err := v.PgDb.Get(&exists, query); err != nil {
+			log.Fatal(err)
+		}
+
+		if !exists {
+			fmt.Println("Parcel not found")
+			return
+		}
+
+		// delete data in jct table
+		query = fmt.Sprintf("delete from %s.t%d_jct where parcel_id = %d;", nrd, yr, parcelId)
+		if _, err := v.PgDb.Exec(query); err != nil {
+			log.Fatal(err)
+		}
+
+		// delete data in parcel table
+		query = fmt.Sprintf("delete from %s.t%d_irr where parcel_id = %d;", nrd, yr, parcelId)
+		if _, err := v.PgDb.Exec(query); err != nil {
+			log.Fatal(err)
+		}
+
+		// tell the user we're done
+		fmt.Println("Deleted parcel with id ", parcelId)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(parcelDeleteCmd)
+	parcelDeleteCmd.Flags().Int("year", 2020, "Year of Parcel to Delete")
+	parcelDeleteCmd.Flags().Int("id", 0, "ID of Parcel to Delete")
+	parcelDeleteCmd.Flags().String("nrd", "", "NRD of Parcel to Delete")
+
+	parcelDeleteCmd.MarkFlagRequired("year")
+	parcelDeleteCmd.MarkFlagRequired("id")
+	parcelDeleteCmd.MarkFlagRequired("nrd")
 }
--- windows_compile.md
+++ windows_compile.md
@@ -4,11 +4,12 @@ Windows takes a couple of special items to compile so the exe can execute. Using
 for C which SQLite is written in and is compiled with GO so that everything is in one binary.
 
 ## Setup
+
 We used the MinGW program for the GCC compiler. This was downloaded and installed from [TDM-gcc](https://jmeubank.github.io/tdm-gcc/) so that we could run the gcc compiler with GO.
 
 ## Steps to compile
+
 - Open the PowerShell App from the start menu and navigate to the dir with the source code
 - To navigate to Z dir type: `Z:\` in prompt
 - Navigate to the directory with source files
 - Run `go build -o bin/wwum2020-amd64-windows.exe` in the prompt
-
